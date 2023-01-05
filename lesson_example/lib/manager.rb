db78c6615d241e296250f53523ff38a0641ac881
Isaac Alter <i_alter@yahoo.com>
2023-01-05 15:13:30 -0700

    Initial commit

--- lesson_example/lib/manager.rb
+++ lesson_example/lib/manager.rb
@@ -1,17 +1,24 @@
-class Manager
+require "./lib/employee"
+
+class Manager < Employee
   attr_reader :base_salary, :per_team_rate, :departments
-  def initialize(base_salary)
+  def initialize(base_salary, name, id)
     @base_salary = base_salary
     @per_team_rate = 5
     @departments = []
+    super(name, id)
   end
 
   def add_department(dept)
     @departments << dept
   end
 
+  def total_compensation
+    @base_salary + (@departments.size * @per_team_rate)
+  end
+
   def benefits
-    [:sick_leave, :health_insurance]
+    super.push(:health_insurance)
   end
 
 end
